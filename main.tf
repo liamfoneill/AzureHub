30e3c511a854ec43f9c462ea63784bb11ed5f39e
Liam F. O'Neill <liamfoneill@outlook.com>
2020-08-10 08:53:36 +0100

    removes alias

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 provider "azurerm" {
   version = "2.22.0"
-  alias = "Development"
   subscription_id = "f1802194-fb29-4974-a84d-132574cb73a0"
   features {}
 }
 
 resource "azurerm_resource_group" "test" {
-    provider = "Development"
     name = "example"
     location = "West Europe"
 }
